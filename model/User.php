24a1b58f2d062dbf5170a5ab5d5b699bf8040e25
Carl <carlos.premi@gmail.com>
2023-03-18 21:00:58 +0100

    test

--- model/User.php
+++ model/User.php
@@ -1,11 +1,23 @@
 <?php
 namespace model;
 
+/*  Une classe représentant un utilisateur
+ * 
+ *  Cette classe permet de créer des objets utilisateurs
+ *  pour les requetes sur la base de données
+ */
 class User
 {
     private $login;
     private $password;
     private $role;
+    /*
+        * Construit un objet User
+        * 
+        * @param string $login Le login de l'utilisateur
+        * @param string $password Le mot de passe de l'utilisateur
+        * @param string $role Le role de l'utilisateur
+        */
 
     public function __construct($login, $password, $role)
     {
@@ -40,5 +52,5 @@ class User
     public function isModerator(): bool {
         return $this->role === 'moderator';
     }
-    
+
 }
